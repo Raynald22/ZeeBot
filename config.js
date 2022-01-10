85b06594dc693da9ac4b6c24c55f6e8e5c3661c8
Reynaldi <44433953+Raynald22@users.noreply.github.com>
2022-01-10 18:59:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.Color = `#2f3136`;
 module.exports = {
   "PREFIX": "?",
-  "TOKEN": "OTI4ODU3MzQ1ODUxODA1ODQ4.Yde30A.vzh7p5YWSY7GSrl24fvk7oldDgI",
+  "TOKEN": "",
   "COLOR": "#2f3136",
   "MONGOURL": "mongodb+srv://raynald22:mardiani22@cluster0.ibry2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 }
